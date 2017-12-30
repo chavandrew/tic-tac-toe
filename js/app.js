5eb1fd799d83cd9a3d6a576f745207faa1285cbc
Andrew Chavez <chavandrew@gmail.com>
2017-12-30 10:47:45 -0600

    changed logic/response text

--- js/app.js
+++ js/app.js
@@ -70,91 +70,91 @@ function checkWinner() {
   console.log('checkWinner');
   if ( $('#1').text() == 'X'  && $('#2').text() == 'X' && $('#3').text() == 'X' ){
     winCounter();
-    alert('win' && player1);
+    alert('Player 1 Wins');
     return;
 
   } else if( $('#4').text() == 'X'  && $('#5').text() == 'X' && $('#6').text() == 'X'){
     winCounter();
-    alert('win');
+    alert(' Player 1 wins');
       return;
 
 
   } else if ( $('#7').text() == 'X'  && $('#8').text() == 'X' && $('#9').text() == 'X'){
     winCounter();
-    alert('win');
+    alert('Player 1 wins');
       return;
 
   } else if ( $('#1').text() == 'X'  && $('#4').text() == 'X' && $('#7').text() == 'X'){
     winCounter();
-    alert('win');
+    alert('Player 1 wins');
       return;
 
   } else if ( $('#2').text() == 'X'  && $('#5').text() == 'X' && $('#8').text() == 'X'){
     winCounter();
-    alert('win');
+    alert('Player 1 wins');
       return;
 
   } else if ( $('#3').text() == 'X'  && $('#6').text() == 'X' && $('#9').text() == 'X'){
     winCounter();
-    alert('win');
+    alert('Player 1 wins');
       return;
 
   } else if ( $('#1').text() == 'X'  && $('#5').text() == 'X' && $('#9').text() == 'X'){
     winCounter()
-    alert('win');
+    alert('Player 1 wins');
       return;
 
   } else if ( $('#3').text() == 'X'  && $('#5').text() == 'X' && $('#7').text() == 'X'){
     winCounter()
-    alert('win');
+    alert('Player 1 wins');
       return;
 
   }
 
-  if ( $('#1').text() == 'O'  && $('#2').text() == 'O' && $('#3').text() == 'X' ){
+  if ( $('#1').text() == 'O'  && $('#2').text() == 'O' && $('#3').text() == 'O' ){
     winCounter()
-    alert('win');
+    alert('Player 2 wins');
       return;
 
   } else if( $('#4').text() == 'O'  && $('#5').text() == 'O' && $('#6').text() == 'O'){
     winCounter()
-    alert('win');
+    alert('Player 2 wins');
       return;
 
 
   } else if ( $('#7').text() == 'O'  && $('#8').text() == 'O' && $('#9').text() == 'O'){
     winCounter()
-    alert('win');
+    alert('Player 2 wins');
       return;
 
   } else if ( $('#1').text() == 'O'  && $('#4').text() == 'O' && $('#7').text() == 'O'){
     winCounter()
-    alert('win');
+    alert('Player 2 wins');
       return;
 
   } else if ( $('#2').text() == 'O'  && $('#5').text() == 'O' && $('#8').text() == 'O'){
     winCounter()
-    alert('win');
+    alert('Player 2 wins');
       return;
 
   } else if ( $('#3').text() == 'O'  && $('#6').text() == 'O' && $('#9').text() == 'O'){
     winCounter()
-    alert('win');
+    alert('Player 2 wins');
       return;
 
   } else if ( $('#1').text() == 'O'  && $('#5').text() == 'O' && $('#9').text() == 'O'){
     winCounter()
-    alert('win');
+    alert('Player 2 wins');
       return;
 
   } else if ( $('#3').text() == 'O'  && $('#5').text() == 'O' && $('#7').text() == 'O'){
 
     winCounter()
-    alert('win');
+    alert('Player 2 wins');
       return;
 
   } else if( clickOnGame >= 9){
-    alert('cat won game')
+    alert('Really? No winner....hit that reset button to the left and someone better win this time!')
     return;
 
   }
